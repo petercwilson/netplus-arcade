400f81fbfd1873d96d5dd2da2c58f613b3a75df5
Peter C. Wilson <pcwilson19@gmail.com>
2026-02-09 17:30:42 +0000

    Add deployment scripts to the :scripts" section of the package.json file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/netplus-arcade',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
